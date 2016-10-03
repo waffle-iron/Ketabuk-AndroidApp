ce6bbcb0b4e5979a75e2849ffec59ee3fec3764c
Karim Mostafa <karim.mostafa.18@gmail.com>
2016-10-04 01:12:35 +0200

    Create initial intro activity presenter

---
+++ app/src/main/java/com/ketabuk/android/activities/IntroActivityPresenter.java
@@ -0,0 +1,29 @@
+package com.ketabuk.android.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.animation.PathInterpolator;
+
+/**
+ * Created by Karim Mostafa on 10/4/16.
+ */
+
+public class IntroActivityPresenter implements IntroActivityPresenterInterface {
+    Context context;
+    IntroActivityInterface introActivityInterface;
+
+    IntroActivityPresenter(Context context, IntroActivityInterface introActivityInterface){
+        this.context = context;
+        this.introActivityInterface = introActivityInterface;
+
+    }
+
+    @Override
+    public void goToURL(String url) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        context.startActivity(i);
+    }
+}
